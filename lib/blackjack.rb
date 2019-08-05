5465a4aeb5db0dbe43563189d247f1644340ee4f
Srikalyan Megati <kalmegati@gmail.com>
2019-08-05 15:42:05 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -28,8 +28,11 @@ def initial_round
   start_total
 end
 
-def hit?
-  
+def hit?(card_total)
+  prompt_user
+  choice = get_user_input
+  if choice == 'h'
+    card_total += deal_card
 end
 
 def invalid_command
